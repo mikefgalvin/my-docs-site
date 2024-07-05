b7a88224a7dbc8dda64980a75cc6ae702ee71c0b
Mike Galvin <mikefgalvin@gmail.com>
2024-07-05 13:23:44 -0700

    initial commit

---
+++ sidebars.js
@@ -0,0 +1,31 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  // Manually creating the sidebar
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: ['introduction', 'installation'],
+    },
+    {
+      type: 'category',
+      label: 'Features',
+      items: ['usage'],
+    },
+  ],
+};
+
+export default sidebars;
