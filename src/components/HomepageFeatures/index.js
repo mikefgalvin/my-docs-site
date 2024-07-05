1479bd22f9229ad47666f162fe5fc17c82b9bc53
Mike Galvin <mikefgalvin@gmail.com>
2024-07-05 16:18:31 -0700

    main page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Workflow Planning',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Define complex workflows using tasks and events with ease using Paraflow.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Database Support',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Paraflow supports MySQL and SQLite for storing internal states, providing flexible database support.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Extensible and Customizable',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Use feature flags to customize your builds and extend Paraflow functionality.
       </>
     ),
   },
